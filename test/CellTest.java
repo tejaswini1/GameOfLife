5c49d1158dc414ef2bf4a2f6e5b920d2228efa0c
Tejaswini Bhosle <tejubhosle123@gmail.com>
2015-06-08 11:45:35 +0530

    Added spec for Cell to return zero if dead

--- test/CellTest.java
+++ test/CellTest.java
@@ -7,12 +7,21 @@ import static org.hamcrest.core.IsEqual.equalTo;
 public class CellTest {
 
     @Test
-    public void shouldReturnDetailsOfCell(){
+    public void shouldReturnOneIfCellIsAlive(){
         Cell cell = new Cell(0, 1, "alive");
 
         int actualDetails = cell.getCellState();
 
         assertThat(actualDetails, is(equalTo((1))));
+    }
+
+    @Test
+    public void shouldReturnZeroIfCellIsNotAlive(){
+        Cell cell = new Cell(0, 1, "dead");
+
+        int actualDetails = cell.getCellState();
+
+        assertThat(actualDetails, is(equalTo((0))));
     }
 
 }
